ba43d20e98404a298b60d42dcacebc471544aff4
Sadeeq499 <muhammadsadeeq11223@gmail.com>
2024-11-17 16:31:55 +0500

    web config file added

--- src/store/apis/apiSlice.js
+++ src/store/apis/apiSlice.js
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    // baseUrl: "http://gs1ksa.org:9021/api",
-    baseUrl: "http://localhost:3000/api",
+    baseUrl: "http://gs1ksa.org:9021/api",
+    // baseUrl: "http://localhost:3000/api",
     // credentials: "include",
     // prepareHeaders: (headers, { getState }) => {
     //   const token = getState().auth.token;
