5395b549a8c396a1a8c91f95b2bff9a0ef7deff9
Sadeeq499 <muhammadsadeeq11223@gmail.com>
2024-12-08 00:50:13 +0500

    member docs table added

--- src/pages/members/ViewMember.jsx
+++ src/pages/members/ViewMember.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { MdArrowBackIosNew } from "react-icons/md";
 import MemberPersonalInfo from "../../components/members/MemberPersonalInfo";
 import OrdersTable from "../../components/members/OrdersTable";
+import MemberDocuments from "../../components/members/MemberDocuments";
 
 function ViewMember() {
   const navigate = useNavigate();
@@ -25,6 +26,9 @@ function ViewMember() {
       </Card>
       <Card className="m-4">
         <OrdersTable />
+      </Card>
+      <Card className="m-4">
+        <MemberDocuments />
       </Card>
     </MainLayout>
   );
